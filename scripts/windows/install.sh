72d90c46413e2a6882f8c56ba999d78938206c13
Gert Kanter <gert.kanter@taltech.ee>
2025-02-03 12:31:52 +0200

    Hotfix two word usernames

--- scripts/windows/install.sh
+++ scripts/windows/install.sh
@@ -9,11 +9,11 @@ SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 echo "Cleaning old install..."
 $SCRIPT_DIR/uninstall.sh
 echo "Creating symlinks..."
-ln -s $SCRIPT_DIR/robot_test /usr/bin
+ln -s "$SCRIPT_DIR/robot_test" /usr/bin
 if [ $? -ne 0 ]; then
     echo "Run this command from 'Git Bash' in Administrator mode"
 else
-    ln -s $SCRIPT_DIR/key_install /usr/bin
+    ln -s "$SCRIPT_DIR/key_install" /usr/bin
     sed -i "s,INSTALL_DIR=INSTALL_DIR_PLACEHOLDER,INSTALL_DIR=$SCRIPT_DIR," /usr/bin/robot_test
     echo "Finished!"
 fi
